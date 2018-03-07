804fe28f088b2d03a5011ea3a131df4177c6b89a
ChosenRealmAlfeche <chosen.alfeche@gmail.com>
2018-03-08 00:32:26 +0800

    adding jovi_chosen remote for last compiler

---
+++ src/ble/BleDriver.java
@@ -0,0 +1,104 @@
+package ble;
+
+// import ble.Http.Server;
+import ble.SyntaxAnalyzer.*;
+import ble.injector.ImbedHtml;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import javax.script.ScriptException;
+
+import Ble.Network.Network;
+import Ble.Network.Http.Server;
+
+/**
+ *
+ * @author Max
+ */
+public class BleDriver {
+    
+    protected Network network = null;
+    
+    static SyntaxAnalyzer sa = new SyntaxAnalyzer();
+    //Temporary for reading every .ble file for http upload to browser
+    private static final File CONSTANT_FOLDER = new File("bledocs/");
+    private static final File[] CONSTANT_LISTOFFILES = CONSTANT_FOLDER.listFiles();
+    
+    
+    
+    public static void main(String[] args) throws IOException, ScriptException, Exception {
+        BleDriver driver = new BleDriver();
+        
+           Server server = null;
+        
+        server = driver.network().createServer(8080);
+        server.start();
+        
+        server = driver.network().createServer(9999);
+        server.start();
+        
+        
+        String bleCode, temp;
+        String[] lines;
+        int i;
+       
+        /*
+        bleCode = new String(Files.readAllBytes(Paths.get("helloworld.ble")), StandardCharsets.UTF_8);
+        
+        if(sa.analyze(bleCode)) {
+            System.out.println("Correct Syntax!");
+            // Getting rid of Comments
+            bleCode = Comments.comments(bleCode);
+            
+            // Check the code line-by-line and check what function to use :(
+            lines = bleCode.split(System.getProperty("line.separator"));
+            
+            for(i = 0; i < lines.length; i++) {
+                MainProcess.process(lines[i]);
+            }
+            
+        } else {
+            System.out.println("Syntax Error");
+        }
+        */
+        
+        //Assume output processed from special task no. 2...
+        String[] results = new String[2];
+        results[0] = "varX = 10";
+        results[1] = "varY = 20";
+        
+        
+        return;
+        
+        //Assume files come from http request or socket...
+        /* int ctr = 8080;
+        for (File file : CONSTANT_LISTOFFILES) {
+                if (file.isFile()) {
+                    System.out.println("...Preparing files for browser upload");
+                    bleCode = new String(Files.readAllBytes(Paths.get("bledocs/"+file.getName())), StandardCharsets.UTF_8);
+                    ImbedHtml injectResult = new ImbedHtml(bleCode, results);
+                    Server server = new Server();
+                    
+                    System.out.println("File: "+file.getName());
+                    injectResult.imbedResults();
+                    server.setContext(file.getName());
+                    server.setResponse(injectResult.getHtmlCode());
+                    server.setSocketNo(ctr);
+                    System.out.println("Located at: localhost:"+server.getSocketNo()+server.getContext());
+                    
+                    Server.server(null);
+                    ctr++;
+                }
+        } */
+    }
+    
+    public BleDriver() {
+        this.network = new Network();
+    }
+    
+    public Network network() {
+        return this.network;
+    }
+}
---
+++ src/ble/Functionalities/MainProcess.java
@@ -0,0 +1,116 @@
+package ble.Functionalities;
+
+import ble.SyntaxAnalyzer.Queue_stack;
+import static ble.SyntaxAnalyzer.SyntaxAnalyzer.ARRAYS;
+import static ble.SyntaxAnalyzer.SyntaxAnalyzer.CONDITIONAL;
+import static ble.SyntaxAnalyzer.SyntaxAnalyzer.COOKIES;
+import static ble.SyntaxAnalyzer.SyntaxAnalyzer.DB;
+import static ble.SyntaxAnalyzer.SyntaxAnalyzer.DISPLAY;
+import static ble.SyntaxAnalyzer.SyntaxAnalyzer.FORLOOP;
+import static ble.SyntaxAnalyzer.SyntaxAnalyzer.REPEAT;
+import static ble.SyntaxAnalyzer.SyntaxAnalyzer.TIME_LIB;
+import static ble.SyntaxAnalyzer.SyntaxAnalyzer.VALIDATION;
+import java.io.IOException;
+import java.util.Queue;
+import java.util.Stack;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import javax.script.ScriptException;
+
+/**
+ *
+ * @author Max
+ */
+public class MainProcess {
+    static Pattern p;
+    static Matcher m;
+    static Queue<Integer> qu;
+    static Stack<Integer> st;
+    
+    public static String process(String line) throws ScriptException, IOException {
+        String status;
+        
+        status = "Ok";
+        
+        p = Pattern.compile(REPEAT);
+        m = p.matcher(line);
+
+        if(m.find()) {
+            Loops.repeat(line);
+        }
+        
+        p = Pattern.compile(FORLOOP);
+        m = p.matcher(line);
+        
+        if(m.find()) {
+            Loops.forLoop(line);
+        }
+
+        p = Pattern.compile(COOKIES);
+        m = p.matcher(line);
+
+        if(m.find()) {
+            status = Cookies.cookies(line);
+        }
+        
+        p = Pattern.compile(DISPLAY);
+        m = p.matcher(line);
+        
+        if(m.find()) {
+            Display.run(line);
+        }
+        
+        p = Pattern.compile(VALIDATION);
+        m = p.matcher(line);
+        
+        if(m.find()) {
+            ValidationFunct.run(line);
+        }
+        
+        p = Pattern.compile(CONDITIONAL);
+        m = p.matcher(line);
+        
+        if(m.find()) {
+            Condi cond = new Condi();
+            cond.evalStatement(line);
+        }
+        
+        p = Pattern.compile(DB);
+        m = p.matcher(line);
+        
+        if(m.find()) {
+            Database.test(line);
+        }
+        
+        p = Pattern.compile(TIME_LIB);
+        m = p.matcher(line);
+        
+        if(m.find()) {
+            TimeLibraryModel t1 = new TimeLibraryModel();
+            t1.checkTimeFunctionality(line);
+        }
+        
+        p = Pattern.compile(Queue_stack.getQueue());
+        m = p.matcher(line);
+        
+        if(m.find()) {
+            Queue1.declareQueue(qu, line);
+        }  
+        
+        p = Pattern.compile(Queue_stack.getStack());
+        m = p.matcher(line);
+        
+        if(m.find()) {
+            Stack1.declareStack(st, line);
+        }
+        
+        p = Pattern.compile(ARRAYS);
+        m = p.matcher(line);
+        
+        if(m.find()) {
+            ArraysFunct.arrayStuff(line);
+        }
+        
+        return status;
+    }       
+}
